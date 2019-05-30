c612698579d3d17ea0ccac0cc88a0cd3fe1a0def
sniukalov <sniukalov@luxoft.com>
2019-05-30 09:32:08 +0300

    Added OnServiceUpdate notification

--- src/components/application_manager/test/include/application_manager/mock_message_helper.h
+++ src/components/application_manager/test/include/application_manager/mock_message_helper.h
@@ -316,6 +316,18 @@ class MockMessageHelper {
   MOCK_METHOD2(BroadcastCapabilityUpdate,
                void(smart_objects::SmartObject& msg_params,
                     ApplicationManager& app_mngr));
+  MOCK_METHOD3(CreateOnServiceStatusUpdateNotification,
+               smart_objects::SmartObject(
+                   const uint32_t app_id,
+                   const hmi_apis::Common_ServiceType::eType service_type,
+                   const hmi_apis::Common_ServiceEvent::eType service_event));
+  MOCK_METHOD4(CreateOnServiceStatusUpdateNotification,
+               smart_objects::SmartObject(
+                   const uint32_t app_id,
+                   const hmi_apis::Common_ServiceType::eType service_type,
+                   const hmi_apis::Common_ServiceEvent::eType service_event,
+                   const hmi_apis::Common_ServiceUpdateReason::eType
+                       service_update_reason));
 
   static MockMessageHelper* message_helper_mock();
 };
