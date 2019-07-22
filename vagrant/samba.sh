58f2c3da3878b96d293916a54e9bd8086f11aa4f
Russell Johnson <russell.johnson@livio.io>
2019-07-22 15:34:28 -0400

    update samba

--- vagrant/samba.sh
+++ vagrant/samba.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
 
-echo "samba"
+echo "samba setup manually
+sudo apt-get install samba
+sudo nano /etc/samba/smb.conf
+[build]
+    comment = Samba on Ubuntu
+    path = /core/build
+    read only = no
+    browsable = yes
+    valid users = vagrant
+
+sudo smbpasswd -a vagrant
+
+sudo service smbd restart
+
+On mac any networked drives will be on /Volumes/
+
+"
