e510d0697bc86ffcf15dfeab3a47ca8bbc563ab8
Russell Johnson <russell.johnson@livio.io>
2019-07-22 12:42:15 -0400

    startup

---
+++ vagrant/sdl_core/startup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+
+t1=${date +%s}
+
+sudo rm /build -R || true
+sudo mkdir /build
+cd /build
+
+cmake -DBUILD_AVAHI_SUPPORT=OFF -DBUILD_USB_SUPPORT=OFF -DBUILD_BT_SUPPORT=OFF \-DEXTENDED_MEDIA_MODE=OFF -DENABLE_HMI_PTU_DECRYPTION=OFF /sdl_core
+
+t2=${date +%s}
+
+
+echo "$t1 $t2 "
+
+d1=t2-t1
+echo "$t1 $t2 "
