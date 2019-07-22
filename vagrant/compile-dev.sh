dede443100416c176790663a1aa304d3e5262b4a
Russell Johnson <russell.johnson@livio.io>
2019-07-22 13:46:24 -0400

    make -j 8 correction

--- vagrant/compile-dev.sh
+++ vagrant/compile-dev.sh
@@ -27,7 +27,7 @@ cd /core/build
 cmake -DBUILD_AVAHI_SUPPORT=OFF -DBUILD_USB_SUPPORT=OFF -DBUILD_BT_SUPPORT=OFF \-DEXTENDED_MEDIA_MODE=OFF -DENABLE_HMI_PTU_DECRYPTION=OFF /core/sdl_core
 
 #there is an initial install of the third party apps that doesn't have the percent and can take a while because it cannot use multiple cores.
-make -j8
+make -j 8
 
 
 t2=$(date +%s)
