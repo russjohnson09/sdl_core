e52634342334220356275fc0af62df3e052d45e4
Russell Johnson <russell.johnson@livio.io>
2019-08-09 14:08:11 -0400

    update settings core

--- vagrant/samba.sh
+++ vagrant/samba.sh
@@ -4,17 +4,69 @@
 echo "samba setup manually
 sudo apt-get install samba
 sudo nano /etc/samba/smb.conf
-[build]
+[build_dev]
     comment = Samba on Ubuntu
     path = /core/build
     read only = no
     browsable = yes
     valid users = vagrant
 
+
+[build_guest]
+    comment = Public files
+    path = /core/build
+    browsable = yes
+    guest ok = yes
+    writable = yes
+    guest account = vagrant
+    create mask = 0775
+    directory mask = 0755
+
+[core_guest]
+    comment = Public files
+    path = /core
+    browsable = yes
+    guest ok = yes
+    writable = yes
+    guest account = vagrant
+    create mask = 0775
+    directory mask = 0755
+
+
 sudo smbpasswd -a vagrant
+
+sudo adduser russ
+sudo smbpasswd -a russ
 
 sudo service smbd restart
 
 On mac any networked drives will be on /Volumes/
 
 "
+
+
+
+#valid users = vagrant, russ
+
+
+
+#https://linuxize.com/post/how-to-install-and-configure-samba-on-ubuntu-18-04/#connecting-to-a-samba-share-from-linux
+sudo apt install smbclient
+
+smbclient //127.0.0.1/build_dev -U vagrant
+
+
+
+#vagrant@sdl-core-develop:/core/build/bin$ smbclient //127.0.0.1/build -U vagrant
+#WARNING: The "syslog" option is deprecated
+#Enter vagrant's password:
+#Domain=[WORKGROUP] OS=[Windows 6.1] Server=[Samba 4.3.11-Ubuntu]
+#tree connect failed: NT_STATUS_BAD_NETWORK_NAME
+
+
+
+smbclient //127.0.0.1/build_dev -U vagrant
+
+
+
+brew install smbclient
--- vagrant/sdl_core_develop/README.md
+++ vagrant/sdl_core_develop/README.md
@@ -1,6 +1,97 @@
-#Startup
+#Start
+
+cd /core/build/bin
+
+
+./start.sh
+
+
+##Connect via hmi
+hmi2.localhost
+
+##Connect App
+###ip of host
+192.168.1.57
+###port
+12345
+
+or
+
+
+
+
+
+#Restart Database
+rm /core/build/bin/storage -R
+./start.sh
+
+
+#Check Samba
+sudo service samaba status
+cat /etc/samba/smb.conf 
+
+##Connect To Samba
+192.168.1.223
+
+build_dev
+vagrant:vagrant
+
+
+
+#Update From Develop From Host
+git checkout russ/vagrant-develop
+
+git pull upstream
+git pull upstream develop
+
+
+##Rebuild on Vagrant Box
 
 cd /core/sdl_core
 
+git checkout russ/vagrant-develop
 
 git pull
+
+
+##Make and install
+cd cd /core/build
+make
+make install
+
+cd bin
+nano log4cxx.properties
+nano sdl_preloaded_pt.json
+
+0x07
+http://192.168.1.57:4005/api/v1/production/policy
+
+
+sudo nano smartDeviceLink.ini
+; WebSocket connection address and port
+ServerAddress = 0.0.0.0
+ServerPort = 8087
+
+
+#Lock Screen Image
+
+https://i.imgur.com/TgkvOIZ.png
+![alt text](https://i.imgur.com/TgkvOIZ.png "Logo Title Text 1")
+
+https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+
+
+
+#Zip ubuntu version and save
+
+
+
+
+cd /core
+vagrant@sdl-core-develop:/core$ du -hs build
+4.5G    build
+
+tar -czvf develop-2019-08-08.tar.gz /core/build
+
+
